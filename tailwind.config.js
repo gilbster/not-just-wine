a57551d9d77765d2490703bee1e66add047293c1
Alexander Gilbertson <alexander.gilbertson@icloud.com>
2022-07-17 15:35:07 -0500

    some slight changes
    added tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,14 @@
 module.exports = {
-  content: ['./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}'],
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('daisyui'),
-  ],
+  content: ["./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}"],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['"Work Sans"', "sans-serif"],
-      }
-    }
+        sans: ['"Work Sans"', "sans-serif"],
+      },
+    },
   },
   daisyui: {
-    themes: [
-      'coffee',
-      'retro',
-    ]
-  }
-}
+    themes: ["coffee", "retro"],
+  },
+};
